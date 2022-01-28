0b3bcc844f84153637560568d90279a08c977bf8
Loisteg <loisteg@outlook.com>
2022-01-29 01:40:45 +0200

    first + second adaptive screens

--- src/js/script.js
+++ src/js/script.js
@@ -17,3 +17,68 @@ const counters = document.querySelectorAll('.work__bar-item__procent'),
 counters.forEach( (item, i) => {
     lines[i].style.width = item.innerHTML;
 });
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 800) {
+        $('.pageup').fadeIn();
+    } else {
+        $('.pageup').fadeOut();
+    }
+});
+
+$("a[href^='#']").click(function(){
+    const _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;
+});
+
+// $('.contacs__form').validate({
+//     rules: {
+//         name: {
+//             required: true,
+//             minlength: 2
+//         },
+//         email: {
+//             required: true,
+//             email: true
+//         },
+//         text: {
+//             required: true,
+//             minlength: 1
+//         }
+//     },
+//     messages: {
+//         name: {
+//             required: 'Пожалуйста, введите своё имя',
+//             minlength: jQuery.validator.format("Введите хотя бы {0} символа")
+//         },
+//         email: {
+//           required: 'Пожалуйста, введите свою почты',
+//           email: 'Неправильно введён адрес почты'
+//         },
+//         text: {
+//             required: ""
+//         }
+//       }
+// });
+
+
+//PHP Form
+
+$('form').submit(function(e) {
+    e.preventDefault();
+
+    if (!$(this).valid()) {
+        return;
+    }
+
+    $.ajax({
+        type: 'POST',
+        url: 'mailer/smart.php',
+        data: $(this).serialize()
+    }).done(function() {
+        $(this).find('input').val('');
+        $('form').trigger('reset');
+    });
+    return false;
+});
