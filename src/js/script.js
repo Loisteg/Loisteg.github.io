8b8cc28d241a2ee3fe8af79ff97d873a207f6b1b
Loisteg <loisteg@outlook.com>
2022-03-15 00:45:15 +0200

    Deleted other animations + added menu__overlay close

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,21 @@
 const hamburger = document.querySelector('.hamburger'),
       menu = document.querySelector('.menu'),
       closeElem = document.querySelector('.menu__close');
+      menuOverlay = document.querySelector('.menu__overlay')
 
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
+    hamburger.style.display = 'none';
+});
+
+menuOverlay.addEventListener('click', () => {
+    menu.classList.remove('active');
+    hamburger.style.display = 'flex';
 });
 
 closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
+    hamburger.style.display = 'flex';
 });
 
 const counters = document.querySelectorAll('.work__bar-item__procent'),
