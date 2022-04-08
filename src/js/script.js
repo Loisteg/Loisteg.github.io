eb03843b9b7992969950e19c4a51d9e92338d9a1
Loisteg <loisteg@outlook.com>
2022-04-08 21:36:50 +0300

    Added normal backgrounds, select language

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,26 @@
 const hamburger = document.querySelector('.hamburger'),
       menu = document.querySelector('.menu'),
-      closeElem = document.querySelector('.menu__close');
-      menuOverlay = document.querySelector('.menu__overlay')
+      closeElem = document.querySelector('.menu__close'),
+      menuOverlay = document.querySelector('.menu__overlay'); 
+
+const select = document.querySelector('.change-lang');  
 
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
     hamburger.style.display = 'none';
+    select.style.display = 'none'
 });
 
 menuOverlay.addEventListener('click', () => {
     menu.classList.remove('active');
     hamburger.style.display = 'flex';
+    select.style.display = 'block'
 });
 
 closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
     hamburger.style.display = 'flex';
+    select.style.display = 'block'
 });
 
 const counters = document.querySelectorAll('.work__bar-item__procent'),
@@ -47,3 +52,38 @@ if (document.body.clientWidth > 767) {
 // Animate with WOW
 
 new WOW().init();
+
+// Language
+const allLanguages = ['en', 'ru', 'ua'];   
+const sidepanelText = document.querySelector('.lng-sidepanel__text');
+
+select.addEventListener('change', changeURLLangauge);
+
+function changeURLLangauge() {
+    let lang = select.value;
+    location.href = `${window.location.pathname}#${lang}`;
+    location.reload();
+}
+
+function changeLanguage() {
+    let hash = window.location.hash.substring(1);
+    if (!allLanguages.includes(hash)) {
+        location.href = `${window.location.pathname}#ua`;
+        location.reload()
+    }
+
+    if (hash === "en") {
+        sidepanelText.style.transform = 'rotate(-90deg) translate(-33%,-200%)';
+        sidepanelText.style.width = '120px';
+    }
+
+    select.value = hash;
+    for (let key in langArr) {
+        let elem = document.querySelector('.lng-' + key);
+        if (elem) {
+            elem.innerHTML = langArr[key][hash];
+        }
+    }
+}
+
+changeLanguage();
